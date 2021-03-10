c2f0135d01947fa34811ff4c80ea44bc88bfe902
AlDvo <Dvorenenko@inbox.ru>
2021-03-10 15:50:49 +0300

    Задание 3 , исправил

--- src/TaskWork/TaskWork1/TaskWork3.java
+++ src/TaskWork/TaskWork1/TaskWork3.java
@@ -3,8 +3,9 @@ package TaskWork.TaskWork1;
 public class TaskWork3 {
     public static void main(String[] args){
         int[] a = {3,7,8,13,54};
-        a[0] = a[a.length-1];
-        a[4] = 3;
+        int[] b = {a[a.length-1],a[a.length-5]};
+        a[0] = b[b.length-2];
+        a[4] = b[b.length-1];;
         System.out.println(a[0]+ a[a.length-3]);
     }
 }
