2d9d001102a78738f89f86e920c95e2b867ac04b
AlDvo <Dvorenenko@inbox.ru>
2021-03-10 15:41:05 +0300

    Задание 2 , исправил

--- src/TaskWork/TaskWork1/TaskWork2.java
+++ src/TaskWork/TaskWork1/TaskWork2.java
@@ -3,6 +3,6 @@ package TaskWork.TaskWork1;
 public class TaskWork2 {
     public static void main(String[] args) {
         double a = 39098.345120;
-        byte b = 120;
+        byte b = 127;
     }
 }
