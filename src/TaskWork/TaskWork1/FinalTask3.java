41ea7ce6c009815b6b2ad3958676ae1279dcb7c0
AlDvo <Dvorenenko@inbox.ru>
2021-03-17 15:50:01 +0300

    Итоговая задача 3

--- src/TaskWork/TaskWork1/FinalTask3.java
+++ src/TaskWork/TaskWork1/FinalTask3.java
@@ -6,17 +6,49 @@ import java.util.Scanner;
 
 public class FinalTask3 {
     public static void main(String[] args) throws IOException {
+        //вводим количество строк n
         Scanner scanner = new Scanner(System.in);
         System.out.print("Количество строк: ");
-        int a = scanner.nextInt();
-        String[] mass = new String[a];
+        int n = scanner.nextInt();
+        //вводим сами строки
+        String[] mass = new String[n];
         int i = 0 ;
-        while(i < a){
+        while(i < n){
             System.out.print("Строка " + (i+1)+" :");
             BufferedReader stroka = new BufferedReader(new InputStreamReader(System.in));
             String slovo = stroka.readLine();
             mass[i] = slovo;
             i++;
+        }
+        //определяем кол-во символов в строке
+        int[] sym = new int[n];// массив где в каждой ячейке указано кол-во не повторяющихся символов
+
+        for(i = 0 ; i < n ; i++){
+            String myString = "";
+            for ( int b = 0 ; b < mass[i].length() ; b++){
+                int bykva = mass[i].indexOf(mass[i].charAt(b));
+                if (bykva == b) {
+                    myString = myString + mass[i].charAt(b);
+                }
+            }
+            sym[i] = myString.length();
+        }
+
+        //определяем  номер строки с максимальным кол-вом символов
+        int max = 0;
+        int povtor = 0;
+        for( i = 1 ; i < n ; i++){
+            if (sym[max] < sym[i]){
+                max = i;
+            }else if (sym[max] == sym[i]){
+               povtor++;
+            }
+        }
+        // Если таких строк будет много(3 строки), то вывести первую.
+        if(povtor < 2){
+        System.out.println("Ответ: " + mass[max]);
+        } else {
+            System.out.println("Ответ: " + mass[0]);
         }
         scanner.close();
     }
