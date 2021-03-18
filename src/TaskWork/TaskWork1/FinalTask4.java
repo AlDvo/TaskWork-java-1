7de8f960a95782eef74ce3322472be28b2d04011
AlDvo <Dvorenenko@inbox.ru>
2021-03-18 14:44:55 +0300

    Итоговое задание 4

--- src/TaskWork/TaskWork1/FinalTask4.java
+++ src/TaskWork/TaskWork1/FinalTask4.java
@@ -1,23 +1,27 @@
 package TaskWork.TaskWork1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.util.Scanner;
 
 public class FinalTask4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
         String otvet = "Заархивированный вирус";
         String podskazka = "Подсказка";
         int mistake = 0;
         int sovet = 0;
+
         for( int i = 0 ; i < 4 ; i++){
             boolean result;
             boolean advice;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ответ:");
-        String otvet1 = scanner.nextLine();
-        result = otvet1.equalsIgnoreCase(otvet);
-        advice = otvet1.equalsIgnoreCase(podskazka);
-        if (result && !advice){
+            BufferedReader stroka = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Ответ:");
+            String otvet1 = stroka.readLine();
+            result = otvet1.equalsIgnoreCase(otvet);
+            advice = otvet1.equalsIgnoreCase(podskazka);
+
+            if (result && !advice){
             System.out.println("Правильно!");
             break;
         } else if (!result && mistake < 2 && !advice && sovet == 0){
@@ -35,7 +39,6 @@ public class FinalTask4 {
             System.out.println("Обидно, приходи в другой раз");
             break;
         }
-            scanner.close();
         }
     }
 }
