d5022597b5f2c9c94424521914598939e03653f7
AlDvo <Dvorenenko@inbox.ru>
2021-03-18 15:17:33 +0300

    Задание 7

--- src/TaskWork/TaskWork1/TaskWork7.java
+++ src/TaskWork/TaskWork1/TaskWork7.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class TaskWork7 {
     public static void main(String[] args) {
+        int x = 30;
+        int y = 12;
+        int z = 2;
         //Пользователь вводит размер массива и данные с клавиатуры в массив
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер массива:");
@@ -14,18 +17,17 @@ public class TaskWork7 {
             mass[i]=b;
         }
         //Сравнить элементы массива с заранее заданными константами x, y, z.
-        int x = 30;
-        int y = 12;
-        int z = 2;
-        int n = 0;
+        boolean same = false;
         for(int i = 0 ; i < mass.length ; i++){
          if (mass[i]==x | mass[i]==y | mass[i]==z){
-             n++;
+             same = true;
          }
         }
         //Если массив содержит хотя бы одну из констант, вывести текст "Данное значение имеется в константах".
-        if (n > 1){
+        if (same){
             System.out.println("Данное значение имеется в константах");}
+        else {
+            System.out.println("Данных значений нет в константах");}
         scanner.close();
     }
 }
